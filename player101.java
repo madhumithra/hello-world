15663b9987a92d0e4f5123c727a79c3150646d91
madhumithra <madhumithramuthukumar@gmail.com>
2017-06-22 21:56:50 +0530

    Create player101.java

---
+++ player101.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class player101
+{
+    public static void main(String ar[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        StringBuffer sb=new StringBuffer(s).reverse();
+        System.out.println(sb);
+        
+    }
+}
