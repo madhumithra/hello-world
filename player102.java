5ad59ee82c3c1069e34b5925912ce51b8cb71b9b
madhumithra <madhumithramuthukumar@gmail.com>
2017-06-29 10:43:00 +0530

    Create player102.java

---
+++ player102.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class player102
+{
+  public static void main(String ar[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int tmp=1;
+    for(int i=1;i<=a;i++)
+    {
+     tmp=tmp*i;
+    }
+    System.out.println(tmp);
+  }
+}
