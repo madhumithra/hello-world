6237a4f9be1f619dbd0adf76ce6e9a6e831a4425
madhumithra <madhumithramuthukumar@gmail.com>
2017-06-29 10:47:57 +0530

    Create player103.java

---
+++ player103.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class player103
+{
+  public static void main(String ar[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    String s=String.valueOf(a);
+    StringBuffer sb=new StringBuffer(s).reverse();
+    System.out.println(sb);
+    
+  }
+}
