ade60b91db7318342411c654084dba867e1f858c
madhumithra <madhumithramuthukumar@gmail.com>
2017-06-29 10:59:36 +0530

    Create player104.java

---
+++ player104.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class player104
+{
+  public static void main(String ar[])
+  {
+    Scanner sc=new Scanner(System.in);
+    String a=sc.next();
+    int s=Integer.valueOf(a);
+    System.out.println(s);
+    
+  }
+}
